15408494d93318d63a52d51e77918510fa8ea697
uma-oo <oumayma.elfahsi99@gmail.com>
2025-11-03 14:34:45 +0100

    add the 8 one

---
+++ StringContains/StringContains.java
@@ -0,0 +1,9 @@
+
+
+
+public class StringContains {
+    public static boolean isStringContainedIn(String subString, String s) {
+        // your code here
+        return s.contains(subString); 
+    }
+}
