153f86947f6ab71781823a59df8391df16858f54
uma-oo <oumayma.elfahsi99@gmail.com>
2025-11-03 14:36:19 +0100

    add some code

--- StringContains/StringContains.java
+++ StringContains/StringContains.java
@@ -1,9 +1,10 @@
 
-
-
 public class StringContains {
     public static boolean isStringContainedIn(String subString, String s) {
+        if (subString == null || s == null) {
+            return false;
+        }
         // your code here
-        return s.contains(subString); 
+        return s.contains(subString);
     }
 }
