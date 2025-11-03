9f6d6cbfd69ba7d3c3a532c9b9c5568769fb8e25
uma-oo <oumayma.elfahsi99@gmail.com>
2025-11-03 17:05:04 +0100

    add some unsure code

---
+++ StringReplace/StringReplace.java
@@ -0,0 +1,9 @@
+
+public class StringReplace {
+    public static String replace(String s, String target, String replacement) {
+        // your code here
+       
+        s.replace(target, replacement); 
+        return s; 
+    }
+}
