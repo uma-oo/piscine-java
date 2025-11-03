dd0bd18668f61654ebbd9cc265c6817cd67ed35a
uma-oo <oumayma.elfahsi99@gmail.com>
2025-11-03 18:33:38 +0100

    add some edits

--- CleanExtract/CleanExtract.java
+++ CleanExtract/CleanExtract.java
@@ -13,12 +13,11 @@ public class CleanExtract {
             Boolean start = false;
             int startIndex = 0;
             int endIndex = 0;
+            String subResult = new String();
             for (int i = 0; i < subString.length(); i++) {
                 // System.out.println(i);
                 if (subString.charAt(i) == '.' && !start) {
-                    // System.out.println("hunaaaa");
                     startIndex = i;
-                    // System.out.println(i);
                     start = true;
                 } else if (start && subString.charAt(i) == '.') {
                     endIndex = i;
@@ -27,16 +26,19 @@ public class CleanExtract {
 
             // System.out.println(startIndex);
             if (start && startIndex < endIndex) {
-                result.add(subString.substring(startIndex + 1, endIndex).trim());
+                subResult = subString.substring(startIndex + 1, endIndex).strip() ;
             }
 
             // System.out.println(startIndex);
             if (start && endIndex == 0) {
-                result.add(subString.substring(startIndex + 1, subString.length() - 1).trim());
+                subResult = subString.substring(startIndex + 1, subString.length() - 1).strip();
             }
 
             if (!start) {
-                result.add(subString.trim());
+                subResult = subString.strip();
+            }
+            if (subResult != "") {
+                result.add(subResult);
             }
 
         }
