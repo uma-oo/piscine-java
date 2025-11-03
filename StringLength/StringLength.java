a153294746a5bcb34c7b23a04c79c93ce1134dff
uma-oo <oumayma.elfahsi99@gmail.com>
2025-11-03 14:22:33 +0100

    add a slight edit

--- StringLength/StringLength.java
+++ StringLength/StringLength.java
@@ -1,9 +1,10 @@
 
-
-
 public class StringLength {
     public static int getStringLength(String s) {
+        if (s == null) {
+            return 0;
+        }
         // your code here
-        return s.length(); 
+        return s.length();
     }
 }
