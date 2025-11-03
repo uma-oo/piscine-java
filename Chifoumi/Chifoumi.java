de2733f903ab54bfbd7d29a3e8574376b5664aee
uma-oo <oumayma.elfahsi99@gmail.com>
2025-11-03 14:15:46 +0100

    add some edits

--- Chifoumi/Chifoumi.java
+++ Chifoumi/Chifoumi.java
@@ -2,18 +2,15 @@
 public class Chifoumi {
     public static ChifoumiAction getActionBeatenBy(ChifoumiAction chifoumiAction) {
         // your code here
-        ChifoumiAction chifoumi;
+        ChifoumiAction chifoumi = chifoumiAction.PAPER;
         switch (chifoumiAction) {
             case ROCK:
                 chifoumi = chifoumiAction.SCISSOR;
                 break;
             case PAPER:
-
                 chifoumi = chifoumiAction.ROCK;
                 break;
             case SCISSOR:
-
-                chifoumi = chifoumiAction.PAPER;
                 break;
         }
         return chifoumi;
