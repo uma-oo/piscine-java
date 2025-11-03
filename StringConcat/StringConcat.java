53e084f356272242a3144c924eeaef59652e3adc
uma-oo <oumayma.elfahsi99@gmail.com>
2025-11-03 14:26:18 +0100

    add a slight edit

--- StringConcat/StringConcat.java
+++ StringConcat/StringConcat.java
@@ -1,6 +1,14 @@
 
 public class StringConcat {
     public static String concat(String s1, String s2) {
+
+        if (s1 == null) {
+            return s2;
+        }
+
+        if (s2 == null) {
+            return s1;
+        }
         // your code here
         return s1 + s1;
     }
