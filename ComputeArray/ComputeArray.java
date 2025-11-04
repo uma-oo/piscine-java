69021a283cde5dbce64fa8c97b0331ee5eaa33ab
uma-oo <oumayma.elfahsi99@gmail.com>
2025-11-04 11:12:23 +0100

    add some code

---
+++ ComputeArray/ComputeArray.java
@@ -0,0 +1,19 @@
+
+public class ComputeArray {
+    public static int[] computeArray(int[] array) {
+
+        int[] result = new int[array.length];
+        // your code here
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] % 3 == 0) {
+                result[i] = array[i] * 5;
+            } else if (array[i] % 3 == 1) {
+                result[i] = array[i] + 7;
+            } else {
+                result[i] = array[i];
+            }
+        }
+
+        return result;
+    }
+}
