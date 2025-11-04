9b69704c15efd5074d1a41bbcfdbbecb5fd4dfd3
uma-oo <oumayma.elfahsi99@gmail.com>
2025-11-04 17:31:51 +0100

    add some cd

--- Cat/Cat.java
+++ Cat/Cat.java
@@ -1,16 +1,17 @@
 
 import java.io.*;
 
-
 public class Cat {
     public static void cat(String[] args) throws IOException {
         // your code here
 
         for (String fileName : args) {
             FileInputStream reader = new FileInputStream(fileName);
-            byte [] buff = new byte[1024];
-            while (reader.read(buff) !=-1) {
-                System.out.write(buff);
+            System.out.write(reader.readAllBytes());
+            int bytesRead;
+            byte[] buff = new byte[1024];
+            while ((bytesRead = reader.read(buff)) != -1) {
+                System.out.write(buff, 0, bytesRead);
             }
 
             reader.close();
