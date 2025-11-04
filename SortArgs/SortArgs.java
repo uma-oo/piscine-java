a326e823276252b7b6c426ac90731171ae420d51
uma-oo <oumayma.elfahsi99@gmail.com>
2025-11-04 13:32:57 +0100

    add some code

--- SortArgs/SortArgs.java
+++ SortArgs/SortArgs.java
@@ -4,6 +4,9 @@ import java.util.stream.Collectors;
 
 public class SortArgs {
     public static void sort(String[] args) {
+        if (args.length ==0) {
+             System.out.print("\n");
+        }
         // your code here
         List<Integer> result = java.util.Arrays.stream(args).map(number -> Integer.valueOf(number)).sorted()
                 .collect(Collectors.toList());
