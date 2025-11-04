f0715bb2d93097d17b55f7120b3ba23438cc5fc2
uma-oo <oumayma.elfahsi99@gmail.com>
2025-11-04 16:48:03 +0100

    why new String works and toString no ;)

--- Cat/Cat.java
+++ Cat/Cat.java
@@ -7,14 +7,14 @@ public class Cat {
         // your code here
 
         for (String fileName : args) {
-            InputStream inputStream = new FileInputStream(fileName);
-            int data; 
-            while ((data = inputStream.read()) != -1) {
-                
-                System.out.print((char)data);
+            FileInputStream reader = new FileInputStream(fileName);
+            byte [] buff = new byte[1024];
+            int bytesRead ; 
+            while ((bytesRead = reader.read(buff))!=-1) {
+                System.out.print(new String(buff, 0, bytesRead));
             }
 
-            inputStream.close();
+            reader.close();
 
         }
 
