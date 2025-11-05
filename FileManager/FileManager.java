9751010fec4108c70e499cd1f7606b3421e3b609
uma-oo <oumayma.elfahsi99@gmail.com>
2025-11-05 14:42:36 +0100

    add some code to see

---
+++ FileManager/FileManager.java
@@ -0,0 +1,27 @@
+
+import java.io.*;
+import java.nio.file.Paths;
+import java.nio.file.Path;
+
+public class FileManager {
+    public static void createFile(String fileName, String content) throws IOException {
+
+        File newFile = new File(fileName);
+        OutputStream writer = new FileOutputStream(newFile);
+        writer.write(content.getBytes());
+        // your code here
+    }
+
+    public static String getContentFile(String fileName) throws IOException {
+        // your code here
+        InputStream reader = new FileInputStream(fileName);
+        byte[] bytes = reader.readAllBytes();
+        return bytes.toString();
+    }
+
+    public static void deleteFile(String fileName) {
+        // your code here
+        Path path = Paths.get(fileName);
+        path.toFile().delete();
+    }
+}
