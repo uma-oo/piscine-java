215b7279b6ebf7df69690e2e29f681738620d6b1
uma-oo <oumayma.elfahsi99@gmail.com>
2025-11-05 14:01:21 +0100

    add code for capitalizeé

---
+++ Capitalize/Capitalize.java
@@ -0,0 +1,31 @@
+
+import java.io.*;
+import java.nio.file.Paths;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Capitalize {
+    public static void capitalize(String[] args) throws IOException {
+        // your code here
+        // let's use the scanner thing as i'm used to it
+
+        Path filePath = Paths.get(args[0]);
+        Scanner scanner = new Scanner(filePath);
+        OutputStream writer = new FileOutputStream(args[1]);
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            String[] words = line.split("\\s+");
+            ArrayList<String> newWords = new ArrayList<>();
+
+            for (String word : words) {
+                if (word.length() > 0) {
+                    newWords.add(Character.toUpperCase(word.charAt(0)) + word.substring(1));
+                } 
+            }
+            String newLine = String.join(" ", newWords);
+            writer.write(newLine.getBytes());
+        }
+
+    }
+}
