ce191a782724caf6f2b0322678473f460d94246c
uma-oo <oumayma.elfahsi99@gmail.com>
2025-11-06 10:18:36 +0100

    make it private

--- StarMass/CelestialObject.java
+++ StarMass/CelestialObject.java
@@ -7,7 +7,7 @@ public class CelestialObject {
     public double y;
     public double z;
     public String name;
-    public int mass;
+    private int mass;
     public static final double KM_IN_ONE_AU = 150_000_000;
 
     public CelestialObject() {
