b28973515c4d66c633410cedad5c2635a7c13e5b
uma-oo <oumayma.elfahsi99@gmail.com>
2025-11-10 16:57:25 +0100

    add a small change

--- AdventureSorcerer/Sorcerer.java
+++ AdventureSorcerer/Sorcerer.java
@@ -23,6 +23,7 @@ public class Sorcerer extends Character implements Healer {
         int count = 1; 
         while (character.currentHealth<=character.getMaxHealth() && count <= this.healCapacity) {
             character.currentHealth += 1; 
+            this.currentHealth -=1; 
             count +=1; 
         }
     }
@@ -36,3 +37,41 @@ public class Sorcerer extends Character implements Healer {
      return  String.format("%s is a sorcerer with %d HP. It can heal %d HP.", this.getName(), this.getCurrentHealth(), this.getHealCapacity()); 
     }
 }
+
+
+
+//   @Test
+//     void objectSorcerer_heal() {
+//         try {
+//             Method heal = Sorcerer.class.getDeclaredMethod("heal", Character.class);
+//             Method takeDamage = Character.class.getDeclaredMethod("takeDamage", int.class);
+
+//             Constructor<Sorcerer> constructor = Sorcerer.class.getConstructor(String.class, int.class, int.class);
+//             Sorcerer dumbledore = constructor.newInstance("Dumbledore", 30, 8);
+//             Sorcerer ronWeasley = constructor.newInstance("Ron Weasley", 20, 1);
+
+//             Method getCurrentHealth = Character.class.getDeclaredMethod("getCurrentHealth");
+
+//             takeDamage.invoke(ronWeasley, 12);
+
+//             assertThat(getCurrentHealth.invoke(ronWeasley))
+//                     .withFailMessage("The HP of Ron before healing should be 8")
+//                     .isEqualTo(8);
+
+//             heal.invoke(dumbledore, ronWeasley);
+
+//             assertThat(getCurrentHealth.invoke(ronWeasley))
+//                     .withFailMessage("The HP of Ron after first healing should be 16")
+//                     .isEqualTo(16);
+
+//             heal.invoke(dumbledore, ronWeasley);
+
+//             assertThat(getCurrentHealth.invoke(ronWeasley))
+//                     .withFailMessage("The HP of Ron after last healing should be 20")
+//                     .isEqualTo(20);
+//         } catch (NoSuchMethodException | InstantiationException | IllegalAccessException | InvocationTargetException e) {
+//             fail("Sorcerer is not correctly defined", e);
+//         }
+//     }
+
+// }
