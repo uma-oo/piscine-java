32aa3b84549070330046f92ee1db512616316fa8
uma-oo <oumayma.elfahsi99@gmail.com>
2025-11-10 17:52:02 +0100

    add a small fix

--- AdventureSorcerer/Sorcerer.java
+++ AdventureSorcerer/Sorcerer.java
@@ -4,7 +4,7 @@
 
 public class Sorcerer extends Character implements Healer {
 
-    private int healCapacity; 
+    private final int healCapacity; 
 
 
     public  Sorcerer(String name , int maxHealth , int healCapacity) {
