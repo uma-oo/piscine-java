e8860627533b99882e3a4c58f603884df5563761
uma-oo <oumayma.elfahsi99@gmail.com>
2025-11-14 16:43:48 +0100

    add some code

--- ListEquals/ListEquals.java
+++ ListEquals/ListEquals.java
@@ -4,6 +4,10 @@ import java.util.List;
 public class ListEquals {
     public static boolean areListsEqual(List<String> list1, List<String> list2) {
 
+        if (list1 == list2 && list1 == null) {
+            return true;
+        }
+
         if (list1 == null || list2 == null) {
             return false;
         }
