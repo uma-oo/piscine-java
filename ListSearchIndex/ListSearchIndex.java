b3348fdfa76963ba0bc0a79f7646f9078e358543
uma-oo <oumayma.elfahsi99@gmail.com>
2025-11-14 16:54:45 +0100

    add some code

--- ListSearchIndex/ListSearchIndex.java
+++ ListSearchIndex/ListSearchIndex.java
@@ -31,7 +31,7 @@ public class ListSearchIndex {
 
         List<Integer> listAll = new ArrayList<>();
         for (int i = 0; i < list.size(); i++) {
-            if (list.get(i) == value) {
+            if (list.get(i).equals(value)) {
                 listAll.add(i);
             }
         }
