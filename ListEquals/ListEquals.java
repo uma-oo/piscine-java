9b609161f987a520c7731b834ccad8e1dec7404a
uma-oo <oumayma.elfahsi99@gmail.com>
2025-11-14 16:41:49 +0100

    add some code

---
+++ ListEquals/ListEquals.java
@@ -0,0 +1,16 @@
+
+
+
+
+
+
+import java.util.List;
+
+public class ListEquals {
+    public static boolean areListsEqual(List<String> list1, List<String> list2) {
+        // your code here
+
+        return list1.equals(list2); 
+    }
+}
+
