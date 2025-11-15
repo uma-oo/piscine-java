874eeaf350e0f893554d6330b6f7a158373db744
uma-oo <oumayma.elfahsi99@gmail.com>
2025-11-15 17:51:01 +0100

    add some fixes

--- WeddingComplex/WeddingComplex.java
+++ WeddingComplex/WeddingComplex.java
@@ -47,6 +47,9 @@ public class WeddingComplex {
             }
             singles.removeAll(toRemove);
             singles.addAll(toAdd);
+
+            toRemove.clear();
+            toAdd.clear();   
             
         }
         
